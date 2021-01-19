4642530a02f6f26dbc09a4ef1bbbc62938a31eba
Zac <zackoch@gmail.com>
2021-01-18 21:54:41 -0600

    turn off track modifications

--- vendor_onboarding/__init__.py
+++ vendor_onboarding/__init__.py
@@ -8,6 +8,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from vendor_onboarding import routes
