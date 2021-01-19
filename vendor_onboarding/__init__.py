1c3eb9d6a3e0715a989e93b208f3719fdb8ee8f8
Zac <zackoch@gmail.com>
2021-01-18 21:53:00 -0600

    basedir for ev

--- vendor_onboarding/__init__.py
+++ vendor_onboarding/__init__.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
