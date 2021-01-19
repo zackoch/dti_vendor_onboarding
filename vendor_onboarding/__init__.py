d64e90ac386463d2b94a3a4ba1cb252f6bc0e2f4
Zac <zackoch@gmail.com>
2021-01-19 00:06:51 -0600

    change static folder name

--- vendor_onboarding/__init__.py
+++ vendor_onboarding/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static-files')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
